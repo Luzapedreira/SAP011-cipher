4a6c8a298b87b6dae0070096547966eab97e1e72
luzapedreira <luza.freitas@gmail.com>
2023-08-07 09:31:37 -0300

    finalizando

--- src/cipher.js
+++ src/cipher.js
@@ -3,50 +3,50 @@ const cipher = {encode, decode
 };
 
 function encode(offset,text){
-  if(typeof parseInt(offset) !== 'number' || typeof text != 'string') {
+  if(typeof parseInt(offset) !== 'number' || typeof text !== 'string') {
     throw TypeError()
   }
-  let msgdecode= ""
+  let msgDecode= ""
   for (let i = 0; i< text.length; i++){
 
+    // Verifica se é maiúscula
     const numberASCII = text.charCodeAt(i);if(numberASCII >= 65 && numberASCII <= 90) {
       const calc = (((numberASCII - 65) + parseInt(offset)) % 26) + 65;
-      msgdecode += String.fromCharCode(calc);
-      } 
-    
-      else if(numberASCII >= 97 && numberASCII <= 122) {
+      msgDecode += String.fromCharCode(calc);
+    } 
+    // Verifica se é minúscula
+    else if(numberASCII >= 97 && numberASCII <= 122) {
       const calc = (((numberASCII - 97) + parseInt(offset)) % 26) + 97;
-      msgdecode += String.fromCharCode(calc);
-      } 
-    
-      else if (numberASCII >= 32 || numberASCII <= 64) {
-      msg += String.fromCharCode(numberASCII);
-      }
-  } return msgdecode;
+      msgDecode += String.fromCharCode(calc);
+    }
+    else if (numberASCII >= 32 || numberASCII <= 64) {
+      msgDecode += String.fromCharCode(numberASCII);
+    }
+  } return msgDecode;
 }
 
 
 function decode(offset,text) {
-  if(typeof parseInt(offset) !== 'number' || typeof text != 'string') {
+  if(typeof parseInt(offset) !== 'number' || typeof text !== 'string') {
     throw TypeError()
   }
-  let msgcode =""
+  let msgCode = ""
   for (let a = 0; a< text.length; a++){
 
     const numberASCII = text.charCodeAt(a);
     if(numberASCII >= 65 && numberASCII <= 90) {
       const calcDecode = (((numberASCII + 65) - parseInt(offset)) % 26) + 65;
-      msgcode += String.fromCharCode(calcDecode);
+      msgCode += String.fromCharCode(calcDecode);
 
     } else if(numberASCII >= 97 && numberASCII <= 122) {
       const calcDecode = (((numberASCII - 97) - parseInt(offset)) % 26) + 97;
-      msgcode += String.fromCharCode(calcDecode);
+      msgCode += String.fromCharCode(calcDecode);
 
     } else if(numberASCII >= 32 || numberASCII <= 64) {
-      msgcode += String.fromCharCode(numberASCII);
+      msgCode += String.fromCharCode(numberASCII);
     }
     
-  } return msgdec;
+  } return msgCode;
 }
 
 export default cipher;
