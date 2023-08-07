f411df737aa8c47a37c20fc6654190dccbff92fc
luzapedreira <luza.freitas@gmail.com>
2023-08-07 12:10:33 -0300

    Updates

---
+++ cipher.js
@@ -0,0 +1,52 @@
+const cipher = {encode, decode
+
+};
+
+function encode(offset,text){
+  if(typeof parseInt(offset) !== 'number' || typeof text !== 'string') {
+    throw TypeError()
+  }
+  let msgDecode= ""
+  for (let i = 0; i< text.length; i++){
+
+    // Verifica se é maiúscula
+    const numberASCII = text.charCodeAt(i);if(numberASCII >= 65 && numberASCII <= 90) {
+      const calc = (((numberASCII - 65) + parseInt(offset)) % 26) + 65;
+      msgDecode += String.fromCharCode(calc);
+    } 
+    // Verifica se é minúscula
+    else if(numberASCII >= 97 && numberASCII <= 122) {
+      const calc = (((numberASCII - 97) + parseInt(offset)) % 26) + 97;
+      msgDecode += String.fromCharCode(calc);
+    }
+    else if (numberASCII >= 32 || numberASCII <= 64) {
+      msgDecode += String.fromCharCode(numberASCII);
+    }
+  } return msgDecode;
+}
+
+
+function decode(offset,text) {
+  if(typeof parseInt(offset) !== 'number' || typeof text !== 'string') {
+    throw TypeError()
+  }
+  let msgCode = ""
+  for (let a = 0; a< text.length; a++){
+
+    const numberASCII = text.charCodeAt(a);
+    if(numberASCII >= 65 && numberASCII <= 90) {
+      const calcDecode = (((numberASCII + 65) - parseInt(offset)) % 26) + 65;
+      msgCode += String.fromCharCode(calcDecode);
+
+    } else if(numberASCII >= 97 && numberASCII <= 122) {
+      const calcDecode = ((((numberASCII - 97) - parseInt(offset)) % 26)+26)%26 + 97;
+      msgCode += String.fromCharCode(calcDecode);
+
+    } else if(numberASCII >= 32 || numberASCII <= 64) {
+      msgCode += String.fromCharCode(numberASCII);
+    }
+    
+  } return msgCode;
+}
+
+export default cipher;
---
+++ index.js
@@ -0,0 +1,46 @@
+import cipher from './cipher.js';
+// cipher.encode (offset, Text)
+// cipher.decode (offset, Text)
+// console.log(cipher);
+// const uppercasestring= string.topUpperCase()
+
+function codificar() {
+  // pegar o offset
+  const offset = document.getElementById('offset').value;
+  // console.log("offset", offset)
+  // Pegar o que está escrito na caixa de texto
+  const msgDecodificada = document.getElementById('msgDecodificada').value;
+  // console.log("msgDecodificada", msgDecodificada)
+  // codificar a mensagem (passando offset e msg original)
+  const codificado = cipher.encode(offset, msgDecodificada)
+  // mostrar msg codificada na segunda caixa de texto
+  document.getElementById('msgCodificada').value = codificado;
+}
+
+const buttonEncode = document.getElementById('buttonEncode')
+buttonEncode.addEventListener('click', codificar)
+
+function decodificar() {
+
+        
+  // pegar o offset 
+  const offset = document.getElementById('offset').value;
+  // console.log("offset", offset)
+
+  // Pegar o que está escrito na caixa de texto codificado
+  const msgcodificada = document.getElementById('msgCodificada').value;
+  // console.log("msgCodificada", msgCodificada)
+
+  // decodificar a mensagem (passando offset e msg codificada)
+  const decodificado = cipher.decode(offset, msgcodificada)
+
+  // mostrar msg decodificada na primeira caixa de texto
+  document.getElementById("msgDecodificada").value = decodificado;
+
+}
+
+const buttonDecode = document.getElementById('buttonDecode')
+buttonDecode.addEventListener('click', decodificar)
+
+
+
